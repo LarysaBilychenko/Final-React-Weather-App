fa54f949442d14475de54be974b26af3749c7fa8
LarysaBilychenko <116347412+LarysaBilychenko@users.noreply.github.com>
2023-01-25 11:49:05 +0100

    added footer with link on GitHub

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Larysa Bilychenko and is{" "}
+        <a href="https://github.com/LarysaBilychenko/Final-React-Weather-App">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }

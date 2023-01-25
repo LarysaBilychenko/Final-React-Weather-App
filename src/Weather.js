79d85b8ca84869d3f4b916e28d1c8d1c882de9cf
LarysaBilychenko <116347412+LarysaBilychenko@users.noreply.github.com>
2023-01-25 12:31:16 +0100

    created html template

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>Kyiv</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Cloudy</li>
+      </ul>
+    </div>
+  );
+}

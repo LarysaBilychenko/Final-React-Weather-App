90f31c1326248e14eebe26d81419193afb083d3c
LarysaBilychenko <116347412+LarysaBilychenko@users.noreply.github.com>
2023-01-25 11:55:02 +0100

    added link to portfolio site

--- src/App.js
+++ src/App.js
@@ -6,8 +6,20 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by Larysa Bilychenko and is{" "}
-        <a href="https://github.com/LarysaBilychenko/Final-React-Weather-App">
+        This project was coded by{" "}
+        <a
+          href="https://incandescent-frangollo-b2685b.netlify.app/index.html"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Larysa Bilychenko
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/LarysaBilychenko/Final-React-Weather-App"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on GitHub
         </a>
       </footer>

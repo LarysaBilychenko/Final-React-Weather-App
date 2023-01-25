f9717c8c20096145c29827e6f190ba081c21a2bf
LarysaBilychenko <116347412+LarysaBilychenko@users.noreply.github.com>
2023-01-25 12:15:50 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://incandescent-frangollo-b2685b.netlify.app/index.html"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Larysa Bilychenko
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/LarysaBilychenko/Final-React-Weather-App"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://incandescent-frangollo-b2685b.netlify.app/index.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Larysa Bilychenko
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/LarysaBilychenko/Final-React-Weather-App"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

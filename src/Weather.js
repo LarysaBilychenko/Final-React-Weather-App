99e899de10ed0d09876c7381cec59fe596b518d9
LarysaBilychenko <116347412+LarysaBilychenko@users.noreply.github.com>
2023-02-02 15:46:02 +0100

    added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -4,11 +4,49 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              className="form-control"
+              type="search"
+              placeholder="Enter a city..."
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+
       <h1>Kyiv</h1>
       <ul>
         <li>Wednesday 07:00</li>
         <li>Cloudy</li>
       </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/snow_light.png"
+            alt="Cloudy"
+            className="float-start"
+          />
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Presipitation: 15%</li>
+            <li>Humidity: 68%</li>
+            <li>Wind: 10km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
